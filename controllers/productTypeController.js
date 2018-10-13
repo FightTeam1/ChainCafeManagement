d3bc5cdafcfb7b4fe0cac22eb170bbeb7c81a347
leviethao <leviethao2@gmail.com>
2018-10-13 11:36:18 +0700

    * modify productType_create_post method, add contraint primary key, unque

--- controllers/productTypeController.js
+++ controllers/productTypeController.js
@@ -1,4 +1,5 @@
 var ProductType = require('../models/productType')
+var async = require('async')
 
 exports.productType_list = function (req, res) {
   ProductType.find()
@@ -12,24 +13,49 @@ exports.productType_create_post = (req, res) => {
   if (!req.body.code) return res.send({isSuccess: false, error: 'Thiếu mã loại sản phẩm'})
   if (!req.body.name) return res.send({isSuccess: false, error: 'Thiếu tên loại sản phẩm'})
 
-  let productType = new ProductType({
-    code: req.body.code,
-    name: req.body.name
-  })
-
-  productType.save((err, newProductType) => {
-    if (err) return res.send({isSuccess: false, error: 'Lưu thất bại'})
+  async.waterfall([
+    (done) => {
+      ProductType.findOne({code: req.body.code})
+      .exec((err, found) => {
+        if (err) return done(err, null)
+        if (found) return done('Mã loại sản phẩm đã tồn tại', null)
+        done(null)
+      })
+    },
+    (done) => {
+      ProductType.findOne({name: req.body.name})
+      .exec((err, found) => {
+        if (err) return done(err, null)
+        if (found) return done('Tên loại sản phẩm đã tồn tại', null)
+        done(null)
+      })
+    },
+    (done) => {
+      let productType = new ProductType({
+        code: req.body.code,
+        name: req.body.name
+      })
+    
+      productType.save((err, newProductType) => {
+        if (err) return done('Lưu thất bại', null)
+        done(null, newProductType)
+      })
+    }
+  ], (err, newProductType) => {
+    if (err) return res.send({isSuccess: false, error: err})
     res.send({isSuccess: true, productType: newProductType})
   })
 }
 
 
 exports.productType_update_put = (req, res) => {
-  if (!req.params.productTypeId) return res.send({isSuccess: false, error: 'Loại sản phẩm không tồn tại'})
+  if (!req.params.code) return res.send({isSuccess: false, error: `Loại sản phẩm ${req.params.code} không tồn tại`})
   if (!req.body.code) return res.send({isSuccess: false, error: 'Thiếu mã loại sản phẩm'})
   if (!req.body.name) return res.send({isSuccess: false, error: 'Thiếu tên loại sản phẩm'})
 
-  ProductType.findByIdAndUpdate(req.params.productTypeId, req.body, {new: true}, (err, updatedProductType) => {
+
+
+  ProductType.findOneAndUpdate({code: req.params.code}, req.body, {new: true}, (err, updatedProductType) => {
     if (err) return res.send({isSuccess: false, error: 'Cập nhật thất bại'})
     res.send({isSuccess: true, productType: updatedProductType})
   })
