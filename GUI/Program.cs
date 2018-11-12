ac0a3a642a4b52132e7e2b6e6a153f446fc9153d
NguyenNguyen <ozero00022@gmail.com>
2018-11-12 07:55:20 +0700

    add form thong ke

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -22,7 +22,7 @@ namespace GUI
             BonusSkins.Register();
             SkinManager.EnableFormSkins();
             UserLookAndFeel.Default.SetSkinStyle("DevExpress Style");
-            Application.Run(new frmLoaiNhanVien());
+            Application.Run(new frmThongKe());
         }
     }
 }
